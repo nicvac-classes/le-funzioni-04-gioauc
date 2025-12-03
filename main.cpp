146abcc9f2bb4e40b45855428001084fe989080c
gioauc <giorgio.auciello@marconibari.edu.it>
2025-12-03 17:20:16 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -1,11 +1,49 @@
 #include <iostream>
 using namespace std;
-
+int input(int x);
+void moltiplicazione();
+int somma(int a, int b);
+int sottrazione(int x);
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    int a, b;
+    int dichiarazA, dichiarazB, sommaA, sottrazioneA;
+
+    dichiarazA = input(a);
+    dichiarazB = input(b);
+    sommaA = somma(a, b);
+    sottrazioneA = sottrazione(a, b);
+    return 0;
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+int input(int x) {
+    int x;
+
+    do {
+        cout << "Inserisci numero positivo" << endl;
+        cin >> valore;
+        if (x <= 0) {
+            cout << "Errore! il numero deve esere positivo. Riprova" << endl;
+        }
+    } while (x <= 0);
+    
+    return x;
+}
+
+void moltiplicazione() {
+}
+
+int somma(int a, int b) {
+    int valore;
+
+    somma = a + b;
+    
+    return valore;
+}
+
+int sottrazione(int x) {
+    int valore;
+
+    somma = (int) (a - b);
+    
+    return valore;
+}
